7d8fcf5d5beb26986c76670032360ffb6d456fd3
ahernande-18 <ahernandez18@ufl.edu>
2025-03-07 22:13:48 -0500

    Update mandelbrot.py
    Added Part 2 Code

--- mandelbrot.py
+++ mandelbrot.py
@@ -36,3 +36,35 @@ def get_escape_time(c: complex, max_iterations: int) -> int | None:
     # If the code reaches this point it is because current_z's magnitude was never above 2, which means it was never
     # guaranteed to tend toward infinity, thus we return None.
     return None
+
+
+# Part 2 
+
+import numpy as np 
+"""Function creates a 2D grid array containing complex numbers evenly spaced between 
+    top_left and (not included) bottom_right.  
+    """
+def get_complex_grid(top_left: complex, bottom_right: complex, step: float) -> np.ndarray:
+   
+    top_left_real = top_left.real # This line and the next line grabs the real part of the complex number
+    bot_right_real = bottom_right.real
+    top_left_imag = top_left.imag #This line and the next line grabs the imaginary part of the complex number
+    bot_right_imag = bottom_right.imag
+    
+    """ Imaginary values changing (decreasing) ties to moving down (the y axis)
+        Real values changing (increasing) ties to moving to the right (the x axis)"""
+    
+    our_real_val = np.arange(top_left_real, bot_right_real, step) #Creates  array of real numbers from top_left_real to bot_right_real
+    our_imag_val = np.arange(top_left_imag, bot_right_imag, -step) #Creates an array of imaginary numbers from top_left_imag to bot_right_imag
+
+    real_nums = our_real_val.reshape(1,-1) #reshapes into 1 row array
+    imag_nums = our_imag_val.reshape(-1,1) #reshapes into 1 column array 
+
+    complex_grid_result = real_nums + 1j*imag_nums #imag_nums multiplied by 1j to make them complex
+
+    return complex_grid_result
+
+
+#Reference: https://stackoverflow.com/questions/24592803/separate-real-and-imaginary-part-of-a-complex-number-in-python
+#Reference: https://saturncloud.io/blog/understanding-the-differences-between-numpy-reshape1-1-and-reshape1-1/#:~:text=When%20you%20use%20reshape(%2D,elements%20in%20your%20original%20array.
+    
